1f2fc7d1ed2e6f0a14f663317b3b4faeb122dc31
Abi Mulya <abimulya.2001@gmail.com>
2020-07-09 20:54:43 +0700

    trim user login

--- index.php
+++ index.php
@@ -6,5 +6,21 @@ use app\Server as connection;
 $login = new login();
 // $login->check_login('{"Nama": "Saya"}');
 $koneksi = new connection();
-print_r($koneksi->get_data_users());
+// print_r($koneksi->get_data_users());
+
+$data_user = array();
+echo "\nSelamat Datang di Aplikasi Terserah lahhh";
+echo "\n===========================================";
+echo "\nUsername : ";
+$username = trim(fgets(STDIN));
+// array_push($data_user['username'], $username);
+$data_user['username'] = $username;
+echo "\nPassword : ";
+$password = trim(fgets(STDIN));
+$data_user['password'] = $password;
+// array_push($data_user['password'], $password);
+print_r($data_user);
+// echo "\n".$username;
+// echo "\n".$password;
+
  ?>
