24d341df517398d6f44af86a04d080b452e7a9dc
Muhammad Suryono <msuryono0@gmail.com>
2020-07-09 21:01:18 +0700

    Update Trim

--- autoload/app/login.php
+++ autoload/app/login.php
@@ -23,7 +23,18 @@ class Login
 
 	public function form_login()
 	{
-		$this->check_login('{"nama": "saya"}');
+		$data_user = array();
+		echo "\nSelamat Datang di Aplikasi Terserah lahhh";
+		echo "\n===========================================";
+		echo "\nUsername : ";
+		$username = trim(fgets(STDIN));
+		// array_push($data_user['username'], $username);
+		$data_user['username'] = $username;
+		echo "\nPassword : ";
+		$password = trim(fgets(STDIN));
+		$data_user['password'] = $password;
+		// array_push($data_user['password'], $password);
+		print_r($data_user);
 	}
 }
 
